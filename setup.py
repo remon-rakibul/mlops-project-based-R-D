5aa64fe2b4949d60e6f62c643721f095a4503647
remon-rakibul <remon.rakibul.star@gmail.com>
2024-10-17 15:50:01 +0600

    built project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.0.0',
+    author='Md. Rakibul Haque',
+    author_email='remon.rakibul.star@gmail.com',
+    packages=find_packages()
+)
---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+
+project_name = "us_visa"
+
+dir_structure = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+
+
+
+for file_path in dir_structure:
+    file_path = Path(file_path)
+    dir_name, file_name = os.path.split(file_path)
+    if dir_name != '':
+        os.makedirs(dir_name, exist_ok=True)
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, 'w') as f:
+            pass
+    else:
+        print(f'already exists at: {file_path}')
